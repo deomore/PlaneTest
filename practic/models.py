2a9feb6dcec65811f63ef395d49400fac9d1700a
deomore <deomoremv@gmail.com>
2023-04-04 22:49:21 +0300

    Models almost done

--- practic/models.py
+++ practic/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Roles(models.Model):
+    role = models.CharField()
+
+
+class User(models.Model):
+    login = models.CharField()
+    password = models.CharField()
+    role = models.ForeignKey(Roles, on_delete=models.CASCADE)
+
+
+class Author(models.Model):
+    nickname = models.CharField()
+    desc = models.CharField()
+    birthday = models.DateField()
+
+
+class Genres(models.Model):
+    genre = models.CharField()
+
+
+class Books(models.Model):
+    name = models.CharField(max_length=30)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.CharField
+    release_date = models.DateField()
+    genre = models.ForeignKey(Genres, on_delete=models.CASCADE)
